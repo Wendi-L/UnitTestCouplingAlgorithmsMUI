004177c9c0f96effe40c936403e79cc4b7ea1993
Wendi-L <wendi.liu1988@icloud.com>
2022-10-10 17:00:04 +0100

    Update Aitken test case

--- Aitken/heat-left.cpp
+++ Aitken/heat-left.cpp
@@ -86,7 +86,7 @@ int main( int argc, char ** argv ) {
     // fetch data from the other solver
     sampler_pseudo_nearest_neighbor1d<double> s1(0.1);
     chrono_sampler_exact1d  s2;
-	algo_aitken1d aitken(0.01,1.0,ptsVluInit);
+	algo_aitken1d aitken(0.01,1.0);
 
      // Print off a hello world message
     printf("Hello world from Left rank %d out of %d MUI processors\n",
@@ -113,6 +113,8 @@ int main( int argc, char ** argv ) {
 
             u[6] = interface.fetch( "u0", 6 * H, t, s1, s2, aitken );
 
+            printf( "Left under relaxation factor at t= %d is %f\n", t, aitken.get_under_relaxation_factor(t));
+            printf( "Left residual L2 Norm at t= %d is %f\n", t, aitken.get_residual_L2_Norm(t));
 
             // calculate 'interior' points
             for ( int i = 1; i <  6; i++ ) v[i] = u[i] + k / ( H * H ) * ( u[i - 1] + u[i + 1] - 2 * u[i] );
--- Aitken/heat-right.cpp
+++ Aitken/heat-right.cpp
@@ -84,7 +84,7 @@ int main( int argc, char ** argv ) {
     // fetch data from the other solver
     sampler_pseudo_nearest_neighbor1d<double> s1(0.1);
     chrono_sampler_exact1d  s2;
-	algo_aitken1d aitken(0.01,1.0,ptsVluInit);
+	algo_aitken1d aitken(0.01,1.0);
 
      // Print off a hello world message
     printf("Hello world from Right rank %d out of %d MUI processors\n",
@@ -106,7 +106,8 @@ int main( int argc, char ** argv ) {
         printf( "Right grid step %d\n", t );
 
             u[4] = interface.fetch( "u", 4 * H, t, s1, s2, aitken );
-
+            printf( "Right under relaxation factor at t= %d is %f\n", t, aitken.get_under_relaxation_factor(t));
+            printf( "Right residual L2 Norm at t= %d is %f\n", t, aitken.get_residual_L2_Norm(t));
             // calculate 'interior' points
             for ( int i = 5; i <  11; i++ ) v[i] = u[i] + k / ( H * H ) * ( u[i - 1] + u[i + 1] - 2 * u[i] );
             // calculate 'boundary' points
